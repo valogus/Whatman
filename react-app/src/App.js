b9bf7f50e0ee1382186c691ef9f4cccf0b8a40a9
Valentin Guschin <112811051+valogus@users.noreply.github.com>
2023-01-12 13:25:12 +0400

    rout fixes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Auth from './components/Auth/Auth.jsx';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const user = useSelector((session)=> session.auth)
+
   return (
     <div className="App">
       <Routes>
+      <Route path='/' element={<Navigate to={user.userId ? '/library' : '/auth'} />} />
         <Route path='/auth' element={<Auth />}/>
       </Routes>
     </div>
--- react-app/src/store/store.js
+++ react-app/src/store/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit'
-import rootReducer from './reducers/auth'
+import rootReducer from './rootReducer'
 
 
 const store = configureStore({
