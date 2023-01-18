663f5cf13bd04b51fe13d12578b402f5cbc3a2f2
Quertte <a.injen90@gmail.com>
2023-01-18 18:05:45 +0300

    style beginning

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,10 +1,10 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const { User } = require('../db/models');
 
 exports.loginUser = async (req, res) => {
   const {
     email,
-    //  password
+    password,
   } = req.body;
   try {
     const user = await User.findOne({ where: { email } });
@@ -12,35 +12,18 @@ exports.loginUser = async (req, res) => {
       res.status(400).send({ message: 'User does not exist' });
       return;
     }
-    // const passCheck = await bcrypt.compare(password, user.password);
-    // if (passCheck) {
-    req.session.currentUserName = user.login;
-    req.session.currentUserId = user.id;
-    req.session.save(() => {
-      res.json({ userName: user.login, userId: user.id });
-    });
-    // } else {
-    //   res.status(500).send({ message: 'Incorrect email or password' });
-    // }
+    const passCheck = await bcrypt.compare(password, user.password);
+    if (passCheck) {
+      req.session.currentUserName = user.login;
+      req.session.currentUserId = user.id;
+      req.session.save(() => {
+        res.json({ userName: user.login, userId: user.id });
+      });
+    } else {
+      res.status(500).send({ message: 'Incorrect email or password' });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
   }
 };
-
-// router.route('/logout')
-//   .get((req, res) => {
-//     const { userId } = req.session;
-
-//     if (userId) {
-//       try {
-//         req.session.destroy();
-//         res.clearCookie('user_uid');
-//         res.sendStatus(200);
-//       } catch (error) {
-//         res.json({ error: error.message });
-//       }
-//     } else {
-//       res.redirect('/');
-//     }
-//   });
